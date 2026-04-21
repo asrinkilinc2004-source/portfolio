41de54fc42add43f9eae84dbd86e72009378c393
Asrin Kilinc <asrin.kilinc@hva.nl>
2026-04-21 17:56:43 +0200

    portfolio update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
-import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import UserNotRegisteredError from '@/components/UserNotRegistered';
 import Home from './pages/Home';
 // Add page imports here
 
